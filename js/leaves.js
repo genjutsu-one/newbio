f5eb9a27133ff94b0cd7a0f00bc5783bdad069a0
vpnsakura <dmitriisupersell@gmail.com>
2026-09-06 21:12:24 +0300

    Update leaves.js

--- js/leaves.js
+++ js/leaves.js
@@ -4,20 +4,48 @@ function leavesAreFalling() {
     return leafTypes[Math.floor(Math.random() * leafTypes.length)];
   }
 
+  var COUNT = 30;
   var div = document.createElement('div');
   div.className = 'leaves';
-  div.innerHTML = `
-    <i class="leaf ${randLeafType()}"></i>
-    <i class="leaf ${randLeafType()}"></i>
-    <i class="leaf-600px ${randLeafType()}"></i>
-    <i class="leaf-768px ${randLeafType()}"></i>
-    <i class="leaf-1024px ${randLeafType()}"></i>
-    <i class="leaf-1280px ${randLeafType()}"></i>
-    <i class="leaf-1366px ${randLeafType()}"></i>
-    <i class="leaf-1600px ${randLeafType()}"></i>
-    <i class="leaf-1800px ${randLeafType()}"></i>
-    <i class="leaf-1920px ${randLeafType()}"></i>`;
+  var html = '';
+  for (var i = 0; i < COUNT; i++) {
+    html += '<i class="leaf ' + randLeafType() + '"></i>';
+  }
+  div.innerHTML = html;
   document.body.insertBefore(div, document.body.childNodes[0]);
 }
 
-document.addEventListener('DOMContentLoaded', leavesAreFalling);
+function leavesBoot() {
+  var leafTypes = ['leaf-type-maple', 'leaf-type-oak'];
+  function randLeafType() {
+    return leafTypes[Math.floor(Math.random() * leafTypes.length)];
+  }
+
+  var bootScreen = document.getElementById('boot-screen');
+  if (!bootScreen) return;
+
+  var COUNT = 45;
+  var div = document.createElement('div');
+  div.className = 'boot-leaves';
+
+  for (var i = 0; i < COUNT; i++) {
+    var leaf = document.createElement('i');
+    leaf.className = 'boot-leaf ' + randLeafType();
+    var size = 14 + Math.random() * 24;
+    var rot = 220 + Math.random() * 260;
+    leaf.style.left = (Math.random() * 100) + '%';
+    leaf.style.width = size + 'px';
+    leaf.style.height = size + 'px';
+    leaf.style.animationDelay = (Math.random() * 0.7) + 's';
+    leaf.style.animationDuration = (1 + Math.random() * 1.2) + 's';
+    leaf.style.setProperty('--boot-rot', rot + 'deg');
+    div.appendChild(leaf);
+  }
+
+  bootScreen.insertBefore(div, bootScreen.firstChild);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  leavesAreFalling();
+  leavesBoot();
+});
