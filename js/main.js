eb5123ec7d8d3fe34edb7a37cbf4663a55052fbc
vpnsakura <dmitriisupersell@gmail.com>
2026-09-06 20:13:35 +0300

    Refactor main.js to remove redundant code

--- js/main.js
+++ js/main.js
@@ -2,26 +2,22 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 import { trackView, loadReviews, setSb } from './db.js';
 import { initUI } from './ui.js';
 import { initMusicPlayer } from './player.js';
-import { initBackground } from './background.js';
 import { bootSequence } from './boot.js';
 import { init3DTilt } from './tilt.js';
 import { initWidget } from './widget.js';
-import { initTheme } from './theme.js';
 
 const sbClient = createClient(SUPABASE_URL, SUPABASE_KEY);
 window._sbClient = sbClient;
 setSb(sbClient);
 
 bootSequence();
-initTheme();
 
 document.addEventListener('DOMContentLoaded', () => {
   trackView();
   setTimeout(loadReviews, 300);
-  
+
   initUI();
   initMusicPlayer();
-  initBackground();
   init3DTilt();
   initWidget();
 
